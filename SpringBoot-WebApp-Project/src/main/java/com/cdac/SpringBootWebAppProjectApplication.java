3d21f9107260ea848fd0c262495951f4a863addb
DNYANESHWAR DAPKAR <100856491+dnyaneshward1997@users.noreply.github.com>
2023-04-19 10:31:55 +0530

    Add files via upload

---
+++ SpringBoot-WebApp-Project/src/main/java/com/cdac/SpringBootWebAppProjectApplication.java
@@ -0,0 +1,14 @@
+package com.cdac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebAppProjectApplication.class, args);
+	}
+
+}
+		
---
+++ SpringBoot-WebApp-Project/src/main/webapp/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Home page</title>
+</head>
+<body>
+	<div align="center">
+		<h1>Welcome to my Spring Boot Web Application...</h1>
+	</div>
+</body>
+</html>
